06be39921ce7ec9a3286dd604161ce7bd73857d7
AHardiment <99344068+AHardiment@users.noreply.github.com>
2022-08-28 13:07:02 +0100

    importing js files into html

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+const canvasElement = document.querySelector('canvas');
+const context = canvasElement.getContext('2d');
+
+const width = canvasElement.width;
+const height = canvasElement.height;
+
+console.log(width);
+console.log(height);
