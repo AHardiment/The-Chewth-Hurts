2ed47dc6fa488c5296a6f5fabd9cd3828ee8bee1
AHardiment <99344068+AHardiment@users.noreply.github.com>
2022-08-31 22:59:30 +0100

    gathered assets
    Gathered assets and built tilemap for game

---
+++ scripts/game.js
@@ -0,0 +1,22 @@
+class Game {
+    constructor() {
+    }
+
+    startGame() {
+        this.drawEverything();
+        this.runLogic();
+        this.loop();
+    }
+
+    drawEverything() {
+
+    }
+
+    runLogic() {
+
+    }
+
+    loop() {
+
+    }
+}
